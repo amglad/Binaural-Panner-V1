d1cd19c4110db552331043fb624c57f463141929
amglad <amglad@icloud.com>
2023-02-28 13:10:04 -0600

    created xcode project

---
+++ BinauralPannerV1/Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class BinauralPannerV1AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    BinauralPannerV1AudioProcessorEditor (BinauralPannerV1AudioProcessor&);
+    ~BinauralPannerV1AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    BinauralPannerV1AudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BinauralPannerV1AudioProcessorEditor)
+};
