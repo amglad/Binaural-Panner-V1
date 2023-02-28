b2d42528c1ee576aa59bfb63d0295f6a40ccc3ed
amglad <amglad@icloud.com>
2023-02-28 13:57:31 -0600

    Add background image

--- BinauralPannerV1/Source/PluginEditor.cpp
+++ BinauralPannerV1/Source/PluginEditor.cpp
@@ -15,7 +15,9 @@ BinauralPannerV1AudioProcessorEditor::BinauralPannerV1AudioProcessorEditor (Bina
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (417, 419);
+    
+    bgImage = juce::ImageCache::getFromMemory(BinaryData::background_png, BinaryData::background_pngSize);
 }
 
 BinauralPannerV1AudioProcessorEditor::~BinauralPannerV1AudioProcessorEditor()
@@ -26,11 +28,8 @@ BinauralPannerV1AudioProcessorEditor::~BinauralPannerV1AudioProcessorEditor()
 void BinauralPannerV1AudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawImageAt(bgImage,0,0);
 }
 
 void BinauralPannerV1AudioProcessorEditor::resized()
--- BinauralPannerV1/Source/PluginEditor.h
+++ BinauralPannerV1/Source/PluginEditor.h
@@ -28,6 +28,9 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     BinauralPannerV1AudioProcessor& audioProcessor;
+    
+    juce::Image bgImage;
+    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BinauralPannerV1AudioProcessorEditor)
 };
